eff5aea77c7d882ec988aa28c070efd627cdab31
Elodie-mbaye <9992@holbertonstudents.com>
2025-01-17 17:33:05 +0100

    infinite

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
 
-    total = 0
+    result = 0
 
-    for arg in sys.argv[1:]:
-        total += int(arg)
-
-        print(total)
+    for i in range(len(sys.argv) - 1):
+        result += int(sys.argv[i + 1])
+    print("{}".format(result))
