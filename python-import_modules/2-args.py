ef7ddd375df25e7815c4be4bcc6cd90b0e2db8e7
Elodie-mbaye <9992@holbertonstudents.com>
2025-01-17 16:54:33 +0100

    print argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    
+
     import sys
     argv = sys.argv
-    argc = len(argv) -1
+    argc = len(argv)-1
 
     if argc == 0:
         print("0 arguments.")
