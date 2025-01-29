91f8dc5a6ed677f528d175b79c2651f52a3f455f
Elodie-mbaye <9992@holbertonstudents.com>
2025-01-29 15:26:11 +0100

    defines a square by: (based on 0-square.py)

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+"""Define a class Square."""
+
+
+class Square:
+    """ Define the size of the square."""
+
+    def __init__(self, size):
+        """ Initialize a new square.
+
+
+        Args:
+        size (int): size of the new square.
+        """
+        self.__size = size
