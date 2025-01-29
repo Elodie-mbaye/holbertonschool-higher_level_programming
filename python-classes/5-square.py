72f29d14e98dd28db6141989b83463cd3d8816e0
Elodie-mbaye <9992@holbertonstudents.com>
2025-01-29 17:37:48 +0100

    defines a square by: (based on 4-square.py)

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" Defines a class Square"""
+
+
 class Square:
-    """ Defines a class Square"""
+    """represents a square.""" 
 
     def __init__(self, size=0):
         """
