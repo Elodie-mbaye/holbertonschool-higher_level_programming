93b5ffa69b0b3a87e0d6772dd747e20cc065192d
Elodie-mbaye <9992@holbertonstudents.com>
2025-02-05 14:15:01 +0100

    Write a class Rectangle that inherits from BaseGeometry (7-base_geometry.py).

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
+""" Module for the BaseGeometry class """
 
-class BaseGeometry:
-    """BaseGeometry class with integer validation."""
-    def integer_validator(self, name, value):
-        """Validates that a value is a positive integer."""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry."""
+    """Class Rectangle which inherite a BaseGeometry."""
     def __init__(self, width, height):
-        """Initializes width and height after validation."""
+        """Initialize a rectangle height and width."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
